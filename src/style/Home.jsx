103e1b9f0c68032de4ce4659ce18827414e07d8b
numair2000 <numairshaikh45@gmail.com>
2023-12-08 22:56:39 +0530

    modified

--- src/style/Home.jsx
+++ src/style/Home.jsx
@@ -1,68 +1,58 @@
+
+
+
+
+
+import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import './TopHeadlines.css';
-
-
+import img1 from '../images/karate1.jpeg'
+import img2 from '../images/gym.jpeg'
+import img3 from '../images/swim.jpeg'
 
 
 export function Home() {
-    return (
+  return (
+    <div>
+    <Carousel>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img1}
+        alt="First slide"
+      />
+      <Carousel.Caption>
         
-    //     <Carousel>
-    //   <Carousel.Item>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img2}
+        alt="Second slide"
+      />
+
+      <Carousel.Caption>
         
-    //     <Carousel.Caption>
-    //       <h3>First slide label</h3>
-    //       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-      
-    //     <Carousel.Caption>
-    //       <h3>Second slide label</h3>
-    //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-     
-    //     <Carousel.Caption>
-    //       <h3>Third slide label</h3>
-    //       <p>
-    //         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-    //       </p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    // </Carousel>
-           
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img3}
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  </div>
+  )
+}
+
+export default Home
    
-            <div>
-                <Carousel>
-          <Carousel.Item>
-           
-            <Carousel.Caption>
-              <h3>Karate</h3>
-              <p>Learn New Skill of Mind & Body Balance</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            
-            <Carousel.Caption>
-              <h3>Kick-Boxing</h3>
-              <p>Learn A new Fighting Skills</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-           
-            <Carousel.Caption>
-              <h3>Mua-Thai</h3>
-              <p>
-               Learn A Advanced Fighting Skills
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-                </div>
+          
 
     
-
-    )
-}
