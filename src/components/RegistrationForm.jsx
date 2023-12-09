a6113496953bc19120470bbb74dadef09b973742
numair2000 <numairshaikh45@gmail.com>
2023-12-09 22:30:23 +0530

    modified

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,113 +1,6 @@
-// import { useState } from "react";
-// import { Button, Container, Form } from "react-bootstrap";
-
-
-// export const RegistrationForm = () => {
-//     const [formData, setFormData] = useState({
-//       name: '',
-//       email: '',
-//       password: '',
-//     });
-  
-//     const handleChange = (e) => {
-//       const { name, value } = e.target;
-//       setFormData({
-//         ...formData,
-//         [name]: value,
-//       });
-//     };
-  
-//     const handleSubmit = async (e) => {
-//       e.preventDefault();
-    
-//       try {
-//         const response = await fetch('http://localhost:3001/register', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify(formData),
-//         });
-    
-//         const data = await response.json();
-    
-//         if (response.ok) {
-//           console.log('Registration successful:', data);
-//           // You can redirect the user or perform other actions after successful registration
-//         } else {
-//           console.error('Registration failed:', data.message);
-//           // Handle registration failure, show an error message, etc.
-//         }
-//       } catch (error) {
-//         console.error('Error during registration:', error);
-//       }
-//     };
-  
-//     return (
-//         <Container className="mt-4">
-//             <h4>Registration Form</h4>
-//             <Form className="mt-5" onSubmit={handleSubmit}>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label >Enter Your Name</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your name"  onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Email Your Age</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your age" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Email address</Form.Label>
-//                     <Form.Control type="email" placeholder="Enter email" onChange={handleChange}/>
-//                 </Form.Group>
-
-//                 <Form.Group className="mb-3" controlId="formBasicPassword">
-//                     <Form.Label>Password</Form.Label>
-//                     <Form.Control type="password" placeholder="Password" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicPassword">
-//                     <Form.Label>Confirm Password</Form.Label>
-//                     <Form.Control type="password" placeholder="Password" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Past Injuries ( If Any)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your past injuries (if any)" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Enter Your Experience (If Any)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your past experience" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Enter Your Height (For Your Specific Diet Plan)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your height" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Enter Your Weight (For Your Specific Diet Plan)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your weight" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Enter Your Food Allergies (If Any)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your food allergies" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicEmail">
-//                     <Form.Label>Enter Your Medical Problems (If Any)</Form.Label>
-//                     <Form.Control type="text" placeholder="Enter your medical problems" onChange={handleChange}/>
-//                 </Form.Group>
-//                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//                     <Form.Check type="checkbox" label="To learn karate" /> 
-//                     <Form.Check type="checkbox" label="To learn to live a disciplined life" />
-//                     <Form.Check type="checkbox" label="To learn karate" />
-//                     <Form.Check type="checkbox" label="To learn karate" />
-//                 </Form.Group>
-//                 <Button variant="primary" type="submit">
-//                     Submit
-//                 </Button>
-//             </Form>
-//         </Container>
-//     );
-// }
 
 import { useEffect, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Table } from "react-bootstrap";
 
 export const RegistrationForm = () => {
   const [formData, setFormData] = useState({
@@ -124,6 +17,41 @@ export const RegistrationForm = () => {
     medicalProblems: '',
     goals: []
   });
+
+  const [registeredStudents, setRegisteredStudents] = useState({
+    name: '',
+    age: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    pastInjuries: '',
+    experience: '',
+    height: '',
+    weight: '',
+    foodAllergies: '',
+    medicalProblems: '',
+    goals: []
+  });
+
+  useEffect(() => {
+    // Fetch the initial list of registered students when the component mounts
+    fetchRegisteredStudents();
+  }, []);
+
+  const fetchRegisteredStudents = async () => {
+    try {
+      const response = await fetch('http://localhost:3302/registeredStudents');
+      const data = await response.json();
+
+      if (response.ok) {
+        setRegisteredStudents(data.students);
+      } else {
+        console.error('Failed to fetch registered students:', data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching registered students:', error);
+    }
+  };
 
   const [registeredCount, setRegisteredCount] = useState(0);
 
@@ -149,10 +77,18 @@ export const RegistrationForm = () => {
 
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+   
+    setRegisteredStudents({
+      ...registeredStudents,
+      [e.target.name]: e.target.value,
+      [e.target.age]: e.target.value,
+      [e.target.eamil]: e.target.value,
+      [e.target.pastInjuries]: e.target.value,
+      [e.target.experience]: e.target.value,
+      [e.target.height]: e.target.value,
+      [e.target.weight]: e.target.value,
+      [e.target.foodAllergies]: e.target.value,
+      [e.target.medicalProblems]: e.target.value
     });
   };
 
@@ -191,6 +127,7 @@ export const RegistrationForm = () => {
        window.alert("Successfully Registered")
         console.log('Registration successful:', data);
         setRegisteredCount((prevCount) => prevCount + 1);
+        setRegisteredStudents([...registeredStudents, data.user]);
         // You can redirect the user or perform other actions after successful registration
       } else {
         console.error('Registration failed:', data.message);
@@ -203,21 +140,23 @@ export const RegistrationForm = () => {
 
   const handleCancelRegistration = async (e) => {
     e.preventDefault();
-
+  
     try {
-      const response = await fetch('http://localhost:3302/cancelRegistration', {
+      const response = await fetch('http://localhost:3302/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ name: formData.name }), // Send only the name for cancellation
       });
-
+  
       const data = await response.json();
-
+  
       if (response.ok) {
         window.alert("Registration Canceled Successfully");
         console.log('Registration canceled successfully:', data);
+        setRegisteredCount((prevCount) => prevCount - 1);
+        setRegisteredStudents([...registeredStudents, data.user]);
         // You can update the UI or perform other actions after cancellation
       } else {
         console.error('Cancellation failed:', data.message);
@@ -227,46 +166,45 @@ export const RegistrationForm = () => {
       console.error('Error during cancellation:', error);
     }
   };
-
   return (
     <Container className="mt-4">
       <h4>Registration Form</h4>
       <Form className="mt-5" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" name="name" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your name" value={registeredStudents.name} name="name" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicAge">
           <Form.Label>Enter Your Age</Form.Label>
-          <Form.Control type="text" placeholder="Enter your age" name="age" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your age" value={registeredStudents.age} name="age" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} />
+          <Form.Control type="email" placeholder="Enter email" value={registeredStudents.email} name="email" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Any Past Injuries</Form.Label>
-          <Form.Control type="text" placeholder="Enter your injuries" name="pastInjuries" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your injuries" value={registeredStudents.pastInjuries} name="pastInjuries" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Any Past Experience</Form.Label>
-          <Form.Control type="text" placeholder="Enter your experience" name="experience" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your experience" value={registeredStudents.experience} name="experience" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your Height</Form.Label>
-          <Form.Control type="text" placeholder="Enter your height" name="height" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your height" value={registeredStudents.height} name="height" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your weight</Form.Label>
-          <Form.Control type="text" placeholder="Enter your weight" name="weight" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your weight" value={registeredStudents.weight} name="weight" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your Food allergies</Form.Label>
-          <Form.Control type="text" placeholder="Enter your allergies" name="foodAllergies" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your allergies" value={registeredStudents.foodAllergies} name="foodAllergies" onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your Mediacal Problems</Form.Label>
-          <Form.Control type="text" placeholder="Enter your Medical Problems" name="medicalProblems" onChange={handleChange} />
+          <Form.Control type="text" placeholder="Enter your Medical Problems" value={registeredStudents.medicalProblems} name="medicalProblems" onChange={handleChange} />
         </Form.Group>
         {/* Add the rest of your Form.Group components here */}
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -288,6 +226,28 @@ export const RegistrationForm = () => {
           Submit
         </Button>
         <p>{registeredCount} candidates have registered so far.</p>
+
+        <Table className="mt-4" striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            {/* ... other table headers */}
+          </tr>
+        </thead>
+        <tbody>
+        {registeredStudents.map((student, index) => (
+      <tr key={index}>
+        <td>{formData.name}</td>
+        <td>{formData.age}</td>
+        <td>{formData.email}</td>
+        {/* ... other table cells */}
+      </tr>
+    ))}
+        </tbody>
+      </Table>
+        
         <Form className="mt-5" onSubmit={handleCancelRegistration}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Enter Your Name for Cancellation</Form.Label>
